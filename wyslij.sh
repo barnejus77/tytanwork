ebbadefa8234ba86b9e9db32c9b66dc92a299315
Marcin <barnejus77@gmail.com>
2026-03-16 20:18:00 +0100

    skrypt

--- wyslij.sh
+++ wyslij.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-echo "--- Rozpoczynam wysyłanie do GitHub ---"
-git add .
-echo "Co dzisiaj zrobiłeś?"
-read opis
-git commit -m "$opis"
-git push
-echo "--- Gotowe! ---"
+echo "--- Rozpoczynam wysyłanie do GitHub jak nie zabraknie neta :-) ---"
+if ping -c 1 google.com; then
+	git add .
+	echo "Co dzisiaj zrobiłeś?"
+	read opis
+	git commit -m "$opis"
+	git push
+	if [$? eq 0]; then 
+		echo "[OK]" Gotowe!
+	else 
+		echo "[ERROR] niestety brak netu"
+	fi
+else 
+	echo "Brak internetu zapis lokalny"
+	git add .
+	echo "co dzisaj zrobiles"
+	read opis
+	git commit -m "$opis"
+	if [$? eq 0]; then 
+                echo "[OK] Gotowe! Zmiany zapisane tylko lokalnie"
+        else
+                echo "[ERROR] masz dzisaj pecha nic nie dziala"
+        fi
+
+fi
+
